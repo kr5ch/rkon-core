1bd05397fbc1f81c336d79797dba3a8ce7ba4922
Kronos <verstraete.marin@gmail.com>
2017-02-15 15:30:16 +0100

    Removed useless serialVersionUID

--- src/net/kronos/rkon/core/ex/MalformedPacketException.java
+++ src/net/kronos/rkon/core/ex/MalformedPacketException.java
@@ -3,8 +3,6 @@ package net.kronos.rkon.core.ex;
 import java.io.IOException;
 
 public class MalformedPacketException extends IOException {
-
-	private static final long serialVersionUID = 4855167794892501538L;
 	
 	public MalformedPacketException(String message) {
 		super(message);
